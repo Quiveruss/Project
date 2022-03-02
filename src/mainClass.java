2593d8c51ef9e867dc329e98e32c27c0a4b2e188
Filip Grzankowski <228741@edu.p.lodz.pl>
2022-03-02 15:58:23 +0100

    nowa metoda policz

---
+++ src/mainClass.java
@@ -0,0 +1,20 @@
+public class mainClass {
+
+    /**
+     * Adds two numbers.
+     * @param a first argument
+     * @param b second argument
+     * @return sum of arguments
+     */
+    static int policz(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Starts the program.
+     * @param args program parameters
+     */
+    public static void main(String[] args) {
+        System.out.println("W pierwszym branchu; 2+3=" + policz(2, 3));
+    }
+}
